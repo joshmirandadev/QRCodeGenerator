712217c3d1102e859e74b85429ff9ce342608f99
Joshua <joshmatimiranda@gmail.com>
2023-01-05 17:11:00 -0600

    fixed double link button, download link functional

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # QRCodeGenerator
+
+SVG for image https://www.flaticon.com/free-icon/qr-code_544587
--- js/script.js
+++ js/script.js
@@ -16,11 +16,15 @@ const onGenerateSubmit = (e) => {
 
     setTimeout(() => {
       hideSpinner();
-      generateQRCode(url, size);
-    }, 2000);
-  }
 
-  console.log(url, size);
+      generateQRCode(url, size);
+
+      setTimeout(() => {
+        const saveUrl = qr.querySelector("img").src;
+        createSaveBtn(saveUrl);
+      }, 50);
+    }, 1000);
+  }
 };
 
 const generateQRCode = (url, size) => {
@@ -41,6 +45,19 @@ const hideSpinner = () => {
 
 const clearUI = () => {
   qr.innerHTML = "";
+  const saveLink = document.getElementById("save-link");
+  if (saveLink) saveLink.remove();
+};
+
+const createSaveBtn = (saveUrl) => {
+  const link = document.createElement("a");
+  link.id = "save-link";
+  link.classList =
+    "bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5";
+  link.href = saveUrl;
+  link.download = "qrcode";
+  link.innerHTML = "Save Image";
+  document.getElementById("generated").appendChild(link);
 };
 
 hideSpinner();
